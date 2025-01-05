a267cf13d21d52fa3a48b0772368d37e33ff26c8
tyufjvbn2 <76784946+tyufjvbn2@users.noreply.github.com>
2025-01-05 13:06:06 +0900

    Time: 22 ms (10.87%), Space: 57.7 MB (10.87%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.ts
@@ -0,0 +1,13 @@
+function separateDigits(nums: number[]): number[] {
+    // early return
+    if(nums.length === 0) {
+        return nums
+    }
+
+    // seperate and concat
+    const numArr: number[] = nums.reduce((arr, num) => {
+        return arr.concat(num.toString().split("").map((str) => parseInt(str)))
+    }, [])
+
+    return numArr
+};
