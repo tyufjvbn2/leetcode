85ac667a3020de97f954ab868267b87c3ef2666f
tyufjvbn2 <76784946+tyufjvbn2@users.noreply.github.com>
2025-02-21 17:10:31 +0900

    Time: 3 ms (72.06%), Space: 57.8 MB (5.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,10 +1,19 @@
 function majorityElement(nums: number[]): number {
+    // 라이브러리 객체 필요함
+    /**
+        {
+            1: 4,
+            2: 3,
+            3: 8
+        }
+     */
+    
     let map: any = new Map()
     let count: number = 0;
     let result: number = 0
 
     for(let i = 0; i<nums.length; i++) {
-        const val: number | undefined = map.get(nums[i])
+        const val: number | undefined = map.get(nums[i]) // object.a ?
 
         if(!val) {
             map.set(nums[i], 1)
