9d11c6c4a778f1a415eed383c4738bec19428bc0
tyufjvbn2 <76784946+tyufjvbn2@users.noreply.github.com>
2025-02-28 14:04:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.ts
@@ -0,0 +1,26 @@
+function intersect(nums1: number[], nums2: number[]): number[] {
+    // nums1 = [...new Set(nums1)]
+    // nums2 = [... new Set(nums2)]
+    const min: number = Math.min(nums1.length, nums2.length)
+    let small: number[] = []
+    let large: number[] = []
+
+    const result = []
+
+    if(nums1.length === min) {
+        small = nums1
+        large = nums2
+    }
+    else {
+        small = nums2
+        large = nums1
+    }
+
+    for(let i =0; i<small.length; i++) {
+        if(large.includes(small[i])) {
+            result.push(small[i])
+        }
+    }
+
+    return result
+};
