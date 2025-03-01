b6c5238ca28e3c8bf516806b76a8e15614eb44e1
tyufjvbn2 <76784946+tyufjvbn2@users.noreply.github.com>
2025-03-01 16:15:45 +0900

    Time: 6 ms (97.9%), Space: 67.2 MB (16.97%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -2,5 +2,21 @@ function containsDuplicate(nums: number[]): boolean {
     const compare: number[] = [...new Set(nums)]
     let result: boolean = nums.length === compare.length ? false : true
 
+    // // for문 순회하면서 비교할 배열에 숫자 넣으면서 비교
+
+    
+    // const compare: number[] = []
+
+    // for(let i =0; i< nums.length; i++) {
+    //     if(!compare.length) {
+    //         compare.push(nums[i])
+    //         continue
+    //     }
+    //     if(compare.include(nums[i])) {
+    //         // 
+    //     }
+
+    // }
+
     return result
 };
