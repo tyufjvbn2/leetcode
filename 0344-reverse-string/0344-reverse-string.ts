620921bb670a63d1251baafb3d56da67bbcb06f8
tyufjvbn2 <76784946+tyufjvbn2@users.noreply.github.com>
2025-03-30 19:57:50 +0900

    Time: 1 ms (53.43%), Space: 62.8 MB (35.77%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.ts
+++ 0344-reverse-string/0344-reverse-string.ts
@@ -2,5 +2,14 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    s.reverse()
+    // with method
+    // s.reverse()
+
+    // other way
+    const newArr: string[] = []
+    for(let i = 0; i<s.length/2; i++) {
+        const el: string = s[i]
+        s[i] = s[s.length -1 - i]
+        s[s.length-1 -i] = el
+    }
 };
